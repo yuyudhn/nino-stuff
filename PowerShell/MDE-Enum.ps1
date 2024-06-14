06c1c72fc230140e8cbdf2c0869a2b8c093ea8bf
Problem Child <yuyudhn@zerobyte.id>
2024-06-14 14:47:43 +0700

    Update MDE Enum

--- PowerShell/MDE-Enum.ps1
+++ PowerShell/MDE-Enum.ps1
@@ -3,34 +3,14 @@
 
 $logName = "Microsoft-Windows-Windows Defender/Operational"
 $eventID = 5007
-$pattern = "HKLM\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths\\([^\s]+)"
-$currentUser = whoami
-
-function Test-PathWriteAccess {
-    param (
-        [string]$path
-    )
-
-    try {
-        $testFile = [System.IO.Path]::Combine($path, [System.IO.Path]::GetRandomFileName())
-        [System.IO.File]::WriteAllText($testFile, "test")
-        Remove-Item $testFile -Force
-        return $true
-    } catch {
-        return $false
-    }
-}
+$pattern = "HKLM\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths\\(.+)"
 
 $allEvents = Get-WinEvent -LogName $logName
-
 $filteredEvents = $allEvents | Where-Object { $_.Id -eq $eventID -and $_.Message -match "Exclusions" }
 
 $filteredEvents | ForEach-Object {
     if ($_.Message -match $pattern) {
-        $actualPath = $matches[1]
-        $writeAccess = Test-PathWriteAccess -path $actualPath
-        $accessMessage = if ($writeAccess) { "has write access" } else { "does not have write access" }
+        $actualPath = $matches[1].Trim('"')
         Write-Output "Path: $actualPath"
-        Write-Output "$currentUser $accessMessage to $actualPath"
     }
 }
